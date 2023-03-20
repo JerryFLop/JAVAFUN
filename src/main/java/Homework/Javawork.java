1559f02baad209e7ee067034438ed434f3299b4f
JerryFlop <JParedes@wil.yearup.org>
2023-03-20 13:12:13 -0400

    commit

--- src/main/java/Homework/Javawork.java
+++ src/main/java/Homework/Javawork.java
@@ -1,7 +1,7 @@
 package Homework;
 
 public class Javawork {
-    public static void printNumbers(){
+    /* public static void printNumbers(){
         for(int y = 45; y >= 1; y-- ){
             System.out.println(y);
 
@@ -9,19 +9,21 @@ public class Javawork {
                 System.out.println();
             }
         }
-    }
+    }*/
     public static void main(String[] args) {
 
-        printNumbers();
+       // printNumbers();
 
 
 
 
-        String FirstName="Jeremiah";
+       String FirstName="Jeremiah";
         String LastName=" Paredes";
         int[] firstArray = {103,23,34,41,75};
         int[] secondArray = {66,75,18,91,120};
         System.out.println(FirstName + LastName);
+
+
 
     }
 }
