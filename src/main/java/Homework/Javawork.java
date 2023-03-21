eb1d6ad54d6fd8174df679cc5d17685a3ce7ba1c
JerryFlop <JParedes@wil.yearup.org>
2023-03-20 20:22:03 -0400

    commit

--- src/main/java/Homework/Javawork.java
+++ src/main/java/Homework/Javawork.java
@@ -1,29 +1,103 @@
 package Homework;
 
+import java.time.Year;
+
 public class Javawork {
-    /* public static void printNumbers(){
-        for(int y = 45; y >= 1; y-- ){
+     /*public static void printNumbers(){
+        for(int y = 45; y >= 1; y--){
             System.out.println(y);
 
-            if( y % 3 ){
-                System.out.println();
-            }
+              if( y % 3==0 && y % 5==0){
+                System.out.println( "yearup");
+            } else if (y % 5==0){
+                System.out.println("up");
+            }  else if (y % 3==0){
+              System.out.println("year");
+           }else { System.out.println( );
+              }
+
+
         }
     }*/
     public static void main(String[] args) {
 
-       // printNumbers();
-
-
-
-
-       String FirstName="Jeremiah";
-        String LastName=" Paredes";
-        int[] firstArray = {103,23,34,41,75};
-        int[] secondArray = {66,75,18,91,120};
+         /*printNumbers();
+          int y =45;
+          int year = 3;
+          int up =5;
+          int yearup = year + up;*/
+        // Question 1
+        String FirstName = "Jeremiah";
+        String LastName = " Paredes";
         System.out.println(FirstName + LastName);
+        //Question 2
+        /* int[] firstArray = {103, 23, 34, 41, 75};
+        int[] secondArray = {66, 75, 18, 91, 120};
+        for (int f = 0; f < firstArray.length; f++) {
+            firstArray[f] = firstArray[3] * secondArray[1];
+        }for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[3] + " ");
+        }*/
+
+
+        //Question 3
+            /*String[] musicians ={"Coldplay","Kanye","slipknot" };
+        System.out.println(musicians);
+         while(musicians){
+            System.out.println();
+        }*/
+
+
+        //Question 4
+
+        //Question 5
+
+        //Question 6
+
+       /* for (int i = 13; i <= 19; i++) {
+            System.out.println(i);
+        }*/
+
+
+        //Question 7
+        /*for(int i = 1; i <= 20;i++){
+
+            if( i%2==1){
+                System.out.println(i);
+            }
+        }
+        */
+        //Question 8
+        /*int grade = 100;
+        char lettergrade = 'D';
+        char passinggrade ='A';
+        char  closegrade ='B';
+        char midgrade ='c';
+        char failed = 'f';
+
+
+        if (( grade <= 90) && (grade <= 100 )) {System.out.println("you got an " + passinggrade );
+        } else if (( grade <= 89 ) && (grade >= 80)) { System.out.println ("you got an " + closegrade);
+        } else if ((grade >= 79) && (grade >= 70)) {
+            System.out.println("you got an " + midgrade);
+        } else if ( (grade >=69 ) && (grade >= 60) ){
+            System.out.println("you got an " + lettergrade);
+        } else { System.out.println(" you got an " + failed + " try agian");
+        }*/
+
+        int[] numbersArray = {2, 34, 7, 48, 0, 13, 24};
+
+        for(int i = 0; i <= 24 ; i++) {
+            System.out.println(i);
+        }
+
+
 
 
 
     }
-}
+
+
+
+
+    }
