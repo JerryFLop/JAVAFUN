7ce2695c6cb4b9592bb6e2cf19f8a1c3fbfeffbe
JerryFlop <JParedes@wil.yearup.org>
2023-05-06 22:43:05 -0400

    commit

--- src/main/java/MethodeMadness/Methodpractices.java
+++ src/main/java/MethodeMadness/Methodpractices.java
@@ -16,16 +16,29 @@ public static double buyCoffee(double dollars){
     return changeLeft;
 }
 public static void myHobbies(String[] hobbies){
+    System.out.println( "My Hobbies are : ");
     for (String i : hobbies)
-        System.out.println(i);   {
+        System.out.println("- "+i);   {
         }
 }
-public static boolean checkWeather(String weatherCondition, int temperature){
+public static String checkWeather(String weatherCondition, int temperature){
+//    String w = weatherCondition;
+//    int t = temperature;
 
-       if(weatherCondition.equals("Hot")  && temperature < 80 )
+    System.out.println("The weather is: " + weatherCondition+ " And the temperature is : " + temperature );
+
+    if(weatherCondition.equals("cot")  && temperature > 80 ){
+        return " Wear shorts ";
+       } else if (weatherCondition.equals("cool") && temperature > 50) {
+        return  " Wear wind breakers ";
+       } else if (weatherCondition.equals("cold") && temperature < 50) {
+        return  " Wear a jacket " ;       }
+           else {
+               return    " No such weather condition";
+       }
 
 
-    return true;
+
 }
 
 
@@ -39,8 +52,7 @@ public static void main(String[] args) {
     System.out.println(buyCoffee(5));
     String[] myhobbies ={"video games", "music","Sports" } ;
     myHobbies(myhobbies);
-
-
+    System.out.print(checkWeather("cool",70));
     }
 
 }
